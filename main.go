7dcecd3676c8ed0c2a390e638249f0ceed5f1fa2
Dominic Motuka <dominic.motuka@gmail.com>
2022-04-17 16:50:06 +0300

    Define global variable with dummy data
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- main.go
+++ main.go
@@ -21,3 +21,9 @@ type Company struct {
 	CEO     string `json:"ceo"`
 	Revenue string `json:"revenue"`
 }
+
+var companies = []Company{
+	{ID: "1", Name: "Dell", CEO: "Michael Dell", Revenue: "92.2 billion"},
+	{ID: "2", Name: "Netflix", CEO: "Reed Hastings", Revenue: "20.2 billion"},
+	{ID: "3", Name: "Microsoft", CEO: "Satya Nadella", Revenue: "320 million"},
+}
